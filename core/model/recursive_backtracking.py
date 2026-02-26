582064cc2ad21addb9d0e13e920a1ac56c02b168
Antoine Rebillard <arebilla@z4r3p7.42lyon.fr>
2026-02-26 15:38:49 +0100

    feat: improve recursive backtracking

--- core/model/recursive_backtracking.py
+++ core/model/recursive_backtracking.py
@@ -4,16 +4,20 @@ import numpy as np
 from numpy.typing import NDArray
 
 from core.model.direction import DX, DY, OPPOSITE, Direction
+from parsing.parsing import MazeSettings
 
 
 class RecursiveBacktrackingGenerator:
-    def __init__(self, height: int, width: int) -> None:
-        self._width = width
-        self._height = height
+    def __init__(self, settings: MazeSettings) -> None:
+        self._settings = settings
 
     def generate(self) -> NDArray[np.int8]:
-        grid = np.full((self._height, self._width), 15, dtype=np.int8)
-        self._carve_passages_from(0, 0, grid)
+        grid = np.full(
+            (self._settings.height, self._settings.width), 15, dtype=np.int8
+        )
+        self._carve_passages_from(
+            self._settings.entry[0], self._settings.entry[1], grid
+        )
         return grid
 
     def _carve_passages_from(
@@ -23,8 +27,8 @@ class RecursiveBacktrackingGenerator:
         for d in directions:
             nx, ny = x + DX[d], y + DY[d]
             if (
-                0 <= ny < self._height
-                and 0 <= nx < self._width
+                0 <= ny < self._settings.height
+                and 0 <= nx < self._settings.width
                 and grid[ny][nx] == 15
             ):
                 grid[y][x] &= ~d
