09c1498009376d69354798a28eb079535697f545
Anselme Bounouar <abounoua@z4r4p3.42lyon.fr>
2026-02-27 14:57:45 +0100

    feat: added a maze generator abstract class to handle polymorphic generation

---
+++ core/model/maze_generator.py
@@ -0,0 +1,9 @@
+from abc import ABC, abstractmethod
+import numpy as np
+from numpy.typing import NDArray
+
+
+class MazeGenerator(ABC):
+    @abstractmethod
+    def generate(self) -> NDArray[np.int8]:
+        pass
--- core/model/recursive_backtracking.py
+++ core/model/recursive_backtracking.py
@@ -6,9 +6,10 @@ from numpy.typing import NDArray
 from core.model.direction import DX, DY, OPPOSITE, Direction
 from core.model.maze_initializer import MazeInitializer
 from parsing.parsing import MazeSettings
+from core.model.maze_generator import MazeGenerator
 
 
-class RecursiveBacktrackingGenerator:
+class RecursiveBacktrackingGenerator(MazeGenerator):
     def __init__(
         self, settings: MazeSettings, initializer: MazeInitializer
     ) -> None:
