fddf5f8a3440a821104ca053c24bcbe49c838bf6
Antoine Rebillard <arebilla@z4r3p7.42lyon.fr>
2026-03-05 11:35:44 +0100

    feat: simplify font export using __init__.py

---
+++ core/view/fonts/__init__.py
@@ -0,0 +1,15 @@
+from .inconsolata_16 import font as inconsolata_16
+from .inconsolata_24 import font as inconsolata_24
+from .inconsolata_32 import font as inconsolata_32
+from .press_start_2p_16 import font as press_start_2p_16
+from .press_start_2p_24 import font as press_start_2p_24
+from .press_start_2p_32 import font as press_start_2p_32
+
+__all__ = [
+    "inconsolata_16",
+    "inconsolata_24",
+    "inconsolata_32",
+    "press_start_2p_16",
+    "press_start_2p_24",
+    "press_start_2p_32",
+]
