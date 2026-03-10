61bd2a725913e9eaa1c9c7133b36a596483efa45
Antoine Rebillard <arebilla@z4r3p7.42lyon.fr>
2026-03-10 14:51:57 +0100

    feat: add algorithm type to mazegen init

--- src/mazegen/__init__.py
+++ src/mazegen/__init__.py
@@ -1,6 +1,6 @@
 from .exporter.maze_exporter import MazeExporter
 from .generator.abstract_grid_generator import AbstractMazeGridGenerator
-from .generator.maze_generator import MazeGenerator
+from .generator.maze_generator import MazeGenerationAlgorithm, MazeGenerator
 from .generator.maze_initializer import MazeInitializer
 from .generator.recursive_backtracking import RecursiveBacktrackingGenerator
 from .models.direction import Direction
@@ -11,6 +11,7 @@ __all__ = [
     "MazeSettings",
     "MazeInitializer",
     "MazeModel",
+    "MazeGenerationAlgorithm",
     "RecursiveBacktrackingGenerator",
     "AbstractMazeGridGenerator",
     "MazeGenerator",
