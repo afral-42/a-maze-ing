861427d91a8ab1e650f6779d5632045820c8a480
Antoine Rebillard <arebilla@z4r3p7.42lyon.fr>
2026-03-16 09:24:36 +0100

    feat: improve kruskal and update readme

--- src/mazegen/generator/kruskal_maze_generator.py
+++ src/mazegen/generator/kruskal_maze_generator.py
@@ -9,27 +9,30 @@ from mazegen.models.direction import DX, DY, OPPOSITE, Direction
 from mazegen.models.maze import MazeModel
 
 
-class Node:
+class Tree:
     def __init__(self) -> None:
-        self.parent: Node | None = None
-        self.tree_height = 0
+        self.parent: Tree | None = None
+        self.rank = 0
 
     @property
-    def root(self) -> Node:
+    def root(self) -> Tree:
         if self.parent is None:
             return self
         return self.parent.root
 
-    def is_connected(self, node: Node) -> bool:
+    def is_connected(self, node: Tree) -> bool:
         return self.root == node.root
 
-    def connect(self, node: Node) -> None:
-        # TODO : finir optim kruskal
-        if self.tree_height <= node.tree_height:
-            self.root.parent = node.root
-            self.tree_height += 1
+    def union_by_rank(self, node: Tree) -> None:
+        self_root = self.root
+        node_root = node.root
+        if self_root.rank > node_root.rank:
+            node_root.parent = self_root
+        elif self_root.rank < node_root.rank:
+            self_root.parent = node_root
         else:
-            node.root.parent = self.root
+            node_root.parent = self_root
+            self_root.rank += 1
 
 
 @dataclass
@@ -40,12 +43,12 @@ class Edge:
 
 
 class KruskalMazeGenerator(AbstractMazeGridGenerator):
-    def init_trees(self, lines: int, cols: int) -> list[list[Node]]:
-        sets: list[list[Node]] = []
+    def init_nodes(self, lines: int, cols: int) -> list[list[Tree]]:
+        sets: list[list[Tree]] = []
         for i in range(lines):
             sets.append([])
             for _ in range(cols):
-                sets[i].append(Node())
+                sets[i].append(Tree())
         return sets
 
     def build_edges(self, maze: MazeModel) -> list[Edge]:
@@ -61,16 +64,16 @@ class KruskalMazeGenerator(AbstractMazeGridGenerator):
     def generate(self) -> NDArray[np.int8]:
         grid = self._initializer.init_maze()
         maze = MazeModel(grid, self._settings)
-        trees = self.init_trees(*grid.shape)
+        nodes = self.init_nodes(*grid.shape)
         edges = self.build_edges(maze)
         random.shuffle(edges)
         for edge in edges:
             nx = edge.x + DX[edge.dir]
             ny = edge.y + DY[edge.dir]
-            tree1 = trees[edge.y][edge.x]
-            tree2 = trees[ny][nx]
-            if not tree1.is_connected(tree2):
-                tree1.connect(tree2)
+            node1 = nodes[edge.y][edge.x]
+            tree2 = nodes[ny][nx]
+            if not node1.is_connected(tree2):
+                node1.union_by_rank(tree2)
                 maze.source[edge.y][edge.x] &= ~edge.dir
                 maze.source[ny][nx] &= ~(OPPOSITE[edge.dir])
                 self._build_steps.append((edge.x, edge.y, edge.dir))
